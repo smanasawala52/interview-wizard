a0192b52b888b761046dc0fab518b76b9fcaf4ad
smanasawala52 <67199826+smanasawala52@users.noreply.github.com>
2024-01-30 20:54:35 +0530

    Add files via upload

---
+++ interview-wizard/src/main/java/com/alpha/interview/interviewhelper/InterviewHelperApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.interview.interviewhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewHelperApplication.class, args);
+	}
+
+}
---
+++ interview-wizard/src/test/java/com/alpha/interview/interviewhelper/InterviewHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.interview.interviewhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
